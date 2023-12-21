ca4eadc19cb8e89d41adc416c254e44f508f6ffc
Priyanka27HS <priyankahs@testvagrant.com>
2023-12-21 13:13:53 +0530

    docker files are added

--- env_docker.sh
+++ env_docker.sh
@@ -1,8 +1,8 @@
 # set up environment variables using a file
 # add the database credentials as well here
 
-export MACHINE=machine1
-export WP_HOST=local
+export MACHINE=docker
+export WP_HOST=ampps
 
 export WC_KEY=ck_ab7ffe77766dc6872232cb548a467582f7cca6d9
 export WC_SECRET=cs_2e6106999cecc3ab913fc1609feb2fec11a431fa
